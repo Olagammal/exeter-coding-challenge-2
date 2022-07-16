c495403fe7868b07af294ebc100c39c21fbd8d32
Olagammal Sivasubramanyan <osolagammal@gmail.com>
2022-07-16 12:52:40 +0530

    chore: setup server

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-const fastify = require('fastify')
+const fastify = require('fastify')({ logger: true })
+
+//contains all the data
+var students = []
+
+fastify.get('/', async (request, reply) => {
+    return { hello: 'world' }
+})
+
+const startServer = async () => {
+    try {
+        await fastify.listen({ port: process.env.PORT || 8000 })
+        console.log('server is running')
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+
+startServer()
+
