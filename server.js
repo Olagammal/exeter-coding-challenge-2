01f71e6ea5e30c6fda8ce77fef70ad88d5056933
Olagammal Sivasubramanyan <osolagammal@gmail.com>
2022-07-16 14:09:47 +0530

    feature: add route to get all the student details

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var students = []
 fastify.post('/add', addDataSchema, async (request, response) => {
     let { studentName, studentID, subject1, subject2, subject3, subject4, subject5 } = request.body
     if (studentName && studentID && subject1 && subject2 && subject3 && subject4 && subject5) {
+        //TODO:add validation for student name
         if (studentName.length >= 3) {
             if (!isNaN(parseFloat(subject1)) && !isNaN(parseFloat(subject2)) && !isNaN(parseFloat(subject3)) && !isNaN(parseFloat(subject4)) && !isNaN(parseFloat(subject5))) {
                 students.push(request.body)
@@ -20,6 +21,10 @@ fastify.post('/add', addDataSchema, async (request, response) => {
     } else {
         response.code(400).header('Content-Type', 'application/json;charset=utf-8').send({ error: 'please send the details in correct format' })
     }
+})
+
+fastify.get('/report', addDataSchema, async (request, response) => {
+    response.code(200).header('Content-Type', 'application/json;charset=utf-8').send({ students })
 })
 
 const startServer = async () => {
