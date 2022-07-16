ad81c8b9466378d0b1b897e7299e68cda5bd8ec2
Olagammal Sivasubramanyan <osolagammal@gmail.com>
2022-07-16 14:06:04 +0530

    feature: create route for adding student details

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const fastify = require('fastify')({ logger: true })
+const addDataSchema = require('./routeConfiguration')
 
 //contains all the data
 var students = []
 
-fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
+fastify.post('/add', addDataSchema, async (request, response) => {
+    let { studentName, studentID, subject1, subject2, subject3, subject4, subject5 } = request.body
+    if (studentName && studentID && subject1 && subject2 && subject3 && subject4 && subject5) {
+        if (studentName.length >= 3) {
+            if (!isNaN(parseFloat(subject1)) && !isNaN(parseFloat(subject2)) && !isNaN(parseFloat(subject3)) && !isNaN(parseFloat(subject4)) && !isNaN(parseFloat(subject5))) {
+                students.push(request.body)
+                response.code(200).header('Content-Type', 'application/json;charset=utf-8').send({ students })
+            } else {
+                response.code(400).header('Content-Type', 'application/json;charset=utf-8').send({ error: 'please enter the subject marks properly' })
+            }
+        } else {
+            response.code(400).header('Content-Type', 'application/json;charset=utf-8').send({ error: "student's name should have more than 3 characters" })
+        }
+    } else {
+        response.code(400).header('Content-Type', 'application/json;charset=utf-8').send({ error: 'please send the details in correct format' })
+    }
 })
 
 const startServer = async () => {
