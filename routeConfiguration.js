1826189ad96571fa357f0dfe5b40c0fa395f3d67
Olagammal Sivasubramanyan <osolagammal@gmail.com>
2022-07-16 13:23:11 +0530

    chore: define schema for all routes

---
+++ routeConfiguration.js
@@ -0,0 +1,103 @@
+const addDataSchema = {
+    schema: {
+        body: {
+            type: 'object',
+            properties: {
+                studentName: { type: 'string' },
+                studentID: { type: 'string' },
+                subject1: { type: 'string' },
+                subject2: { type: 'string' },
+                subject3: { type: 'string' },
+                subject4: { type: 'string' },
+                subject5: { type: 'string' }
+            }
+        },
+        response: {
+            201: {
+                type: 'object',
+                properties: {
+                    students: { type: 'object' }
+                }
+            },
+            400: {
+                type: 'object',
+                properties: {
+                    error: { type: 'string' }
+                }
+            }
+        }
+    }
+}
+
+const updateDataSchema = {
+    schema: {
+        body: {
+            type: 'object',
+            properties: {
+                studentID: { type: 'string' },
+                subjectName: { type: 'string' }
+            }
+        },
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    students: { type: 'object' }
+                }
+            },
+            400: {
+                type: 'object',
+                properties: {
+                    error: { type: 'string' }
+                }
+            }
+        }
+    }
+}
+
+const deleteDataSchema = {
+    schema: {
+        body: {
+            type: 'object',
+            properties: {
+                studentID: { type: 'string' }
+            }
+        },
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    students: { type: 'object' }
+                }
+            },
+            404: {
+                type: 'object',
+                properties: {
+                    error: { type: 'string' }
+                }
+            }
+        }
+    }
+}
+
+const getDataSchema = {
+    schema: {
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    students: { type: 'object' }
+                }
+            },
+            404: {
+                type: 'object',
+                properties: {
+                    error: { type: 'string' }
+                }
+            }
+        }
+    }
+}
+
+
+module.exports = { addDataSchema, updateDataSchema, deleteDataSchema, getDataSchema }
